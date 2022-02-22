394ad8578e53bbf3937cc0b23b848dd5fda0c128
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2022-02-22 08:47:42 +0100

    aze

--- routes.php
+++ routes.php
@@ -1,3 +1,6 @@
 <?php
 $r->get('/')->name('home')->file('home')->func('home');
 $r->get('/posts/{id}')->name('get_post_by_id')->file('posts')->func('get_by_id');
+$r->get('/vue-d-ensemble')->name('vue_d_ensemble')->file('home')->func('vueDEnsemble');
+$r->get('/budgets')->name('get_budgets')->file('budgets')->func('get_all');
+$r->post('/add-budget')->name('add_budgets')->file('budgets')->func('add_budget');
